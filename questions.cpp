0b946042aa21cf63c60771806e4c07d1fbeb62e7
Denis Nazarovs <denis.nazarovs@va.lv>
2023-12-05 22:11:22 +0200

    Integrated functions from questions

--- questions.cpp
+++ questions.cpp
@@ -5,7 +5,8 @@
 #include <algorithm>
 #include <random>
 
-struct Question {
+struct Question
+{
     std::string question;
     std::string answer;
     std::string category;
@@ -14,12 +15,15 @@ struct Question {
     std::vector<std::string> answers;
 };
 
-void addAnswers(Question& question, const std::vector<Question>& allQuestions) {
+void addAnswers(Question &question, const std::vector<Question> &allQuestions)
+{
     question.answers.push_back(question.answer);
 
     std::vector<std::string> otherAnswers;
-    for (const auto& q : allQuestions) {
-        if (q.answer != question.answer) {
+    for (const auto &q : allQuestions)
+    {
+        if (q.answer != question.answer)
+        {
             otherAnswers.push_back(q.answer);
         }
     }
@@ -28,39 +32,47 @@ void addAnswers(Question& question, const std::vector<Question>& allQuestions) {
     std::mt19937 g(rd());
     std::shuffle(otherAnswers.begin(), otherAnswers.end(), g);
 
-    for (size_t i = 0; i < 2; ++i) {
+    for (size_t i = 0; i < 2; ++i)
+    {
         question.answers.push_back(otherAnswers[i]);
     }
 
     std::shuffle(question.answers.begin(), question.answers.end(), g);
 
     int index = 1;
-    for (const auto& a : question.answers) {
+    for (const auto &a : question.answers)
+    {
         std::cout << index << ". " << a << std::endl;
         ++index;
     }
-
 }
 
-bool checkAnswer(const Question& question, int userChoice) {
+bool checkAnswer(const Question &question, int userChoice)
+{
     return question.answer == question.answers[userChoice - 1];
 }
 
-std::vector<Question> findQuestions(const std::vector<Question>& questions, const std::string& category, const std::string& difficulty, const std::string& world) {
+std::vector<Question> findQuestions(const std::vector<Question> &questions, const std::string &category, const std::string &difficulty, const std::string &world)
+{
     std::vector<Question> result;
-    for (const auto& q : questions) {
-        if (q.category == category && q.difficulty == difficulty && q.world == world) {
+    for (const auto &q : questions)
+    {
+        if (q.category == category && q.difficulty == difficulty && q.world == world)
+        {
             result.push_back(q);
-            //askQuestion(q);
+            // askQuestion(q);
 
             int userChoice;
             std::cout << "Enter the number of your answer: ";
             std::cin >> userChoice;
 
-            if (checkAnswer(q, userChoice)) {
+            if (checkAnswer(q, userChoice))
+            {
                 std::cout << "Correct! You earned 10 points." << std::endl;
                 // userScore += 10; // Add a variable to keep track of the user's score
-            } else {
+            }
+            else
+            {
                 std::cout << "Incorrect. The correct answer is: " << q.answer << std::endl;
             }
         }
@@ -69,9 +81,8 @@ std::vector<Question> findQuestions(const std::vector<Question>& questions, cons
     return result;
 }
 
-
-
-int main() {
+vector<Question> setupVector()
+{
     std::string csv = "libs/country_questions.csv";
 
     rapidcsv::Document doc(csv);
@@ -79,8 +90,8 @@ int main() {
     size_t numRows = doc.GetRowCount();
     std::vector<Question> questions;
 
-
-    for (size_t i = 0; i < numRows; ++i) {
+    for (size_t i = 0; i < numRows; ++i)
+    {
         Question q;
         q.question = doc.GetCell<std::string>("Question", i);
         q.answer = doc.GetCell<std::string>("Answer", i);
@@ -89,14 +100,15 @@ int main() {
         q.world = doc.GetCell<std::string>("World", i);
 
         questions.push_back(q);
-        
     }
 
-     for (auto& q : questions) {
+    for (auto &q : questions)
+    {
         addAnswers(q, questions);
     }
 
-    for (auto& q : questions) {
+    for (auto &q : questions)
+    {
         std::cout << "Question: " << q.question << std::endl;
         std::cout << "Answer: " << q.answer << std::endl;
         std::cout << "Category: " << q.category << std::endl;
@@ -105,9 +117,6 @@ int main() {
         std::cout << "Answers: " << q.answers.size() << std::endl;
         std::cout << "---------------------------" << std::endl;
     }
-    
-    return 0;
+
+    return questions;
 }
-
-
-
